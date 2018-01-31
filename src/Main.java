47f3e037751f901f654e6561b46fd9e07cd7db50
Peter Stanton <stantp2@uw.edu>
2018-01-30 21:43:36 -0800

    Done?

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final String baseGeocode = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-    private static final String googleAPI = "AIzaSyAXOUJZME49BZfMWw3XGCqdrZ0-2MvQj6U";
+    private static final String googleKey = "AIzaSyAXOUJZME49BZfMWw3XGCqdrZ0-2MvQj6U";
     private static final String baseWeather = "https://api.darksky.net/forecast/";
     private static final String weatherKey = "6695e2cfcb5944be3b28b2bcbe4cce57";
+    private static final String divider = "*****************************************************************************************";
 
     public static void main(String[] args) {
-        System.out.println("*****************************************************************************************");
+        System.out.println(divider);
         System.out.println("Welcome to Peter Stanton's CSS490 Weather application!");
         System.out.println("Powered by Google and Dark Sky");
         System.out.println("https://darksky.net/poweredby/");
@@ -33,7 +30,7 @@ public class Main {
             String[] parsed = input.split("\\W+");
             URL googleURL = processGeoURL(parsed);
             String geoResult = processResponse(googleURL);
-            ArrayList<String> geoSplit = new ArrayList<String>(Arrays.asList(geoResult.split("\n")));
+            ArrayList<String> geoSplit = new ArrayList<>(Arrays.asList(geoResult.split("\n")));
             int geoLatIndex = -1;
             int geoLongIndex = -1;
             for (int i = 0; i < geoSplit.size(); i++) {
@@ -58,41 +55,132 @@ public class Main {
         //stuff here.
         String[] results = weatherResult.split("\\{");
         StringBuilder output = new StringBuilder();
-        String[] curWeather;
-        String[] minutelyWeather;
-        String[] hourlyWeather;
-        output.append("Weather for: " + locale + "\n");
-        for(int i = 0; i < results.length; i++) {
+        List<String> curWeather = new ArrayList<>();
+        List<String> minutelyWeather = new ArrayList<>();
+        List<String> hourlyWeather = new ArrayList<>();
+        List<String> alerts = new ArrayList<>();
+        List<String> getURL = new ArrayList<>();
+        URL warningURL = null;
+        output.append('\n' + "Weather for: " + locale + "\n");
+        for (int i = 0; i < results.length; i++) {
             String cur = results[i];
-            if(cur.contains("currently")) {
-                output.append("Timezone: " + cur.split(",")[2].split(":")[1] + "\n");
-                output.append("--------------------------------------------------------------------");
-                curWeather = results[i+1].split(",");
-            }
-            else if(cur.contains("minutely")) {
-                minutelyWeather = results[i+1].split(",");
-            }
-            else if(cur.contains("hourly")) {
-                hourlyWeather = results[i+1].split(",");
+            if (cur.contains("currently")) {
+                output.append("Timezone: " +
+                        cur.split(",")[2].split(":")[1].replace("\"", "") + "\n");
+                output.append(divider + '\n');
+                curWeather = Arrays.asList(results[i + 1].split(","));
+            } else if (cur.contains("minutely")) {
+                minutelyWeather = Arrays.asList(results[i + 1].split(","));
+            } else if (cur.contains("hourly")) {
+                hourlyWeather = Arrays.asList(results[i + 1].split(","));
+            } else if (cur.contains("alerts")) {
+                alerts = Arrays.asList(results[i + 1].split(":"));
+                getURL = Arrays.asList(results[i + 1].split(","));
+                break;
             }
         }
-        output.append("Overview");
+        for (int i = 0; i < getURL.size(); i++) {
+            String checker = getURL.get(i);
+            if (checker.contains("uri")) {
+                try {
+                    warningURL = new URL(checker.split("\"")[3].replace("\"", ""));
+                } catch (MalformedURLException e) { }
+            }
+        }
+        double preIntense = -1.0;
+        double preChance = -1.0;
+        double temp = -10000.00;
+        double windspeed = -1.0;
+        double visibility = -1.0;
 
 
-        //should loop through with a switch tree and on switch, stuff values in a string builder, then
-        //spit it out when I'm done.
+        for (int i = 0; i < curWeather.size(); i++) {
+            String curWeatherChecker = curWeather.get(i);
+            if (curWeatherChecker.contains("precipIntensity")) {
+                preIntense = Double.parseDouble(curWeatherChecker.split(":")[1]);
+            } else if (curWeatherChecker.contains("precipProbability")) {
+                preChance = Double.parseDouble(curWeatherChecker.split(":")[1]);
+            } else if (curWeatherChecker.contains("temperature")) {
+                temp = Double.parseDouble(curWeatherChecker.split(":")[1]);
+            } else if (curWeatherChecker.contains("windSpeed")) {
+                windspeed = Double.parseDouble(curWeatherChecker.split(":")[1]);
+            } else if (curWeatherChecker.contains("visibility")) {
+                visibility = Double.parseDouble(curWeatherChecker.split(":")[1]);
+            }
+        }
+        if(preIntense >= 0.0) {
+            output.append("Precipitation intensity is " + preIntense + " millimeters per hour " + '\n');
+        }
+        if(preChance >= 0.0) {
+            output.append("Chance of continuing precipitation is " + preChance + '\n');
+        }
+        if(temp >= -10000) {
+            output.append("Current local temperature is " + temp + " degrees Fahrenheit" + '\n');
+        }
+        if(windspeed >= 0.0) {
+            output.append("Current windspeed is " + windspeed + " meters per second" + '\n');
+        }
+        if(visibility >= 0.0) {
+            output.append("Current visibility is " + visibility + " kilometers" + '\n');
+        }
+        output.append(divider + '\n');
+
+        String rightnow = "";
+        for (int i = 0; i < minutelyWeather.size(); i++) {
+            String minWeatherChecker = minutelyWeather.get(i);
+            if (minWeatherChecker.contains("summary")) {
+                rightnow = minWeatherChecker.split(":")[1].replace("\"", "");
+            }
+        }
+        if (!rightnow.isEmpty()) {
+            output.append("Right now the weather is: " + rightnow + '\n');
+        }
+        String inFuture = "";
+        for (int i = 0; i < hourlyWeather.size(); i++) {
+            String hourWeatherChecker = hourlyWeather.get(i);
+            if (hourWeatherChecker.contains("summary")) {
+                inFuture = hourWeatherChecker.split(":")[1].replace("\"", "");
+            }
+        }
+        if (!inFuture.isEmpty()) {
+            output.append("In the near future it will be: " + inFuture + '\n');
+        }
+        if (!alerts.isEmpty()) {
+            String aTitle = "";
+            String regions = "";
+            String advisoryType = "";
+            String aDescription = "";
+
+            for (int i = 0; i < alerts.size(); i++) {
+                String alertChecker = alerts.get(i);
+                if (alertChecker.contains("title")) {
+                    aTitle = alerts.get(i + 1).split(",")[0].replace("\"", "");
+                    regions = alerts.get(i + 2).replace("severity", "");
+                    advisoryType = alerts.get(i + 3).replace("\"", "");
+                } else if (alertChecker.contains("description")) {
+                    aDescription = alerts.get(i + 1);
+                }
+            }
+            output.append(divider + '\n');
+            output.append(aTitle + '\n');
+            output.append("Affected regions:" + '\n');
+            output.append(regions + '\n');
+            output.append("The government has classified this alert as a: " + advisoryType + '\n');
+            output.append("Official warning follows" + '\n');
+            output.append(aDescription + '\n');
+            output.append(warningURL);
+            output.append('\n');
+        }
+        output.append(divider + '\n');
+        System.out.println(output.toString());
     }
 
-
-    //more stuff here.
     private static URL processGeoURL(String[] parsed) {
         String joined = String.join("+", parsed);
-        joined += "&key=" + googleAPI;
+        joined += "&key=" + googleKey;
         try {
             return new URL(baseGeocode + joined);
-        } catch (MalformedURLException e) {
-            //blah
-        }
+        } catch (MalformedURLException e) { }
         return null;
     }
 
@@ -109,12 +197,8 @@ public class Main {
 
                 outResult = builder.toString();
                 return outResult;
-            } catch (IOException e) {
-                //blah
-            }
-        } catch (IOException e) {
-            //blah
-        }
+            } catch (IOException e) { }
+        } catch (IOException e) { }
         return null;
     }
 
@@ -122,9 +206,7 @@ public class Main {
         String joined = baseWeather + weatherKey + '/' + geoLat + ',' + geoLong;
         try {
             return new URL(joined);
-        } catch (MalformedURLException e) {
-            //blah
-        }
+        } catch (MalformedURLException e) { }
         return null;
     }
 }
